93c7df7027af2e360ebeece7e5134c853f64e1c8
Sedar <desiradjysedar@gmail.com>
2023-04-07 13:14:43 +0200

    first commit2

--- class/pdo/PdoConnexion.php
+++ class/pdo/PdoConnexion.php
@@ -9,27 +9,27 @@ class PdoConnexion
 
     public function __construct(){
         /*lancement : mysql -h 192.168.22.48 -u john -p
-        machine perso :
+        machine perso :  */
          $db_name = "recettecuisine" ;
         $db_host = '127.0.0.1' ;
         $db_port = '3306' ;
         $db_user =  'root' ;
         $db_pwd = '' ;
+        /*
+                machine fac :
+                $db_name = "lrandria" ;
+                $db_host = '192.168.22.48' ;
+                $db_port = '3306' ;
+                $db_user =  'lrandria' ;
+                $db_pwd = '06032003' ;
 
-        machine fac :
-        $db_name = "lrandria" ;
-        $db_host = '192.168.22.48' ;
-        $db_port = '3306' ;
-        $db_user =  'lrandria' ;
-        $db_pwd = '06032003' ;
+
+                $db_name = "lrandria" ;
+                $db_host = '192.168.22.48' ;
+                $db_port = '3306' ;
+                $db_user =  'lrandria' ;
+                $db_pwd = '06032003' ;
         */
-
-        $db_name = "lrandria" ;
-        $db_host = '192.168.22.48' ;
-        $db_port = '3306' ;
-        $db_user =  'lrandria' ;
-        $db_pwd = '06032003' ;
-
         $dsn = 'mysql:dbname=' . $db_name . ';host='. $db_host. ';port=' . $db_port;
         try{
             $this->pdo = new PDO($dsn, $db_user, $db_pwd);
