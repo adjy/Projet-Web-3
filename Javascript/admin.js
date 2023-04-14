79e97a79f44e4bccda81373b2dff72e90896c0a9
Sedar <desiradjysedar@gmail.com>
2023-04-14 08:01:51 +0200

    tag button

--- Javascript/admin.js
+++ Javascript/admin.js
@@ -1,73 +1,85 @@
-let boutonIngredient= document.getElementById("ajouter-ingredient-button");
+function ajoutIngredients(){
+    let boutonIngredient= document.getElementById("ajouter-ingredient-button");
 
-let liste = document.getElementById('listeIngredient');
-boutonIngredient.addEventListener("click", function(event){
-    
-    // pour le nom de l'ingredient
-    let nom = document.getElementById("nom-ingredient"); // recupre le nom du formulaire
-    if(nom.value === "")
-        return;
-    // let nodeNom = document.createElement("input"); // creation d'un input
-    // nodeNom.name = "nom-ingredient[]"; // ajoute ce nom dans la liste de name nom-ingredient
-    // nodeNom.value = nom.value; // donne comme valeur la valeur de nom
-    // nodeNom.type = "hidden"; // met le input a hidden pour ne pas l'afficher
-    let divNodeNom = document.createElement("div");
-    divNodeNom.innerHTML =  nom.value;
-    // liste.append(nodeNom);
-    nom.value = "";
-    console.log(nom)
-   
+    let liste = document.getElementById('listeIngredient');
+    boutonIngredient.addEventListener("click", function(event){
+        // pour le nom de l'ingredient
+        let nom = document.getElementById("nom-ingredient"); // recupre le nom du formulaire
+        if(nom.value === "")
+            return;
 
-    // divNodeNom.classList.add("aff-ingredient");
-
-    // pour la qte
-    let qte = document.getElementById("qte"); // recupre le nom du formulaire
-    // let nodeqte = document.createElement("input"); // creation d'un input
-    // nodeqte.name = "quantite[]"; // ajoute ce nom dans la liste de name nom-ingredient
-    // nodeqte.value = qte.value; // donne comme valeur la valeur de nom
-    // nodeqte.type = "hidden"; // met le input a hidden pour ne pas l'afficher
-    let divNodeQte = document.createElement("div");
-    divNodeQte.innerHTML =  qte.value;
-    // liste.append(nodeqte);
-    qte.value="";
-    //
-    //
-    // //pour l'unite
-    let unite = document.getElementById("unite"); // recupre le nom du formulaire
-    // let nodeunite = document.createElement("input"); // creation d'un input
-    // nodeunite.name = "unite[]"; // ajoute ce nom dans la liste de name nom-ingredient
-    // nodeunite.value = unite.value; // donne comme valeur la valeur de nom
-    // nodeunite.type = "hidden"; // met le input a hidden pour ne pas l'afficher
-    let divNodeUnite = document.createElement("div");
-    divNodeUnite.innerHTML =  unite.value;
-    // liste.append(nodeunite);
-    unite.value="";
-   //
-   //  // pour la photo de l'ingredient
-    let photo = document.getElementById("photo_ingredient"); // recupre le nom du formulaire
-    let nodePhoto = document.createElement("file"); // creation d'un input
-    nodePhoto.name = "photo_ingredient[]"; // ajoute ce nom dans la liste de name nom-ingredient
-    nodePhoto.value = photo.value; // donne comme valeur la valeur de nom
-    nodePhoto.type = "hidden"; // met le input a hidden pour ne pas l'afficher
+        let divNodeNom = document.createElement("div");
+        divNodeNom.innerHTML =  nom.value;
+        nom.value = "";
 
 
-    let divIngredient = document.createElement("div");// creation d'un div pour afficher l'ingredient ajouter
-    divIngredient.append(divNodeNom);
-    divIngredient.append(divNodeQte);
-    divIngredient.append(divNodeUnite);
-   //
-   //
-   liste.append(divIngredient);
 
-    
-})
+        // pour la qte
+        let qte = document.getElementById("qte"); // recupre le nom du formulaire
+        let divNodeQte = document.createElement("div");
+        divNodeQte.innerHTML =  qte.value;
+        qte.value="";
 
-document.addEventListener("DOMContentLoaded", function (event){
+        // //pour l'unite
+        let unite = document.getElementById("unite"); // recupre le nom du formulaire
+        let divNodeUnite = document.createElement("div");
+        divNodeUnite.innerHTML =  unite.value;
+        unite.value="";
+
+        //  // pour la photo de l'ingredient
+        let photo = document.getElementById("photo_ingredient"); // recupre le nom du formulaire
+        let nodePhoto = document.createElement("file"); // creation d'un input
+        nodePhoto.name = "photo_ingredient[]"; // ajoute ce nom dans la liste de name nom-ingredient
+        nodePhoto.value = photo.value; // donne comme valeur la valeur de nom
+        nodePhoto.type = "hidden"; // met le input a hidden pour ne pas l'afficher
+
+
+        let divIngredient = document.createElement("div");// creation d'un div pour afficher l'ingredient ajouter
+        divIngredient.append(divNodeNom);
+        divIngredient.append(divNodeQte);
+        divIngredient.append(divNodeUnite);
+
+        liste.append(divIngredient);
+
+    })
+
+}
+
+function ajoutTags(){
+    let boutonTags= document.getElementById("ajouter-tag-button");
+
+    let liste = document.getElementById('listeTags');
+    boutonTags.addEventListener("click", function(event){
+        // pour le nom de l'ingredient
+        let nom = document.getElementById("nom-tag"); // recupre le nom du formulaire
+        if(nom.value === "")
+            return;
+
+        let divNodeNom = document.createElement("div");
+        divNodeNom.innerHTML =  nom.value;
+        nom.value = "";
+
+        let divIngredient = document.createElement("div");// creation d'un div pour afficher l'ingredient ajouter
+        divIngredient.append(divNodeNom);
+        liste.append(divIngredient);
+
+    })
+
+}
+
+function submitRecette(){
     let form = document.getElementById("ajout-recette-form") ;
     let button = document.getElementById("ajout-recette")
 
     button.addEventListener('click', function (e){
         form.submit()
     })
+}
+
+document.addEventListener('DOMContentLoaded',function (){
+
+    ajoutIngredients();
+    submitRecette();
+    ajoutTags();
 
 })
--- class/recette/AjoutFormulaire.php
+++ class/recette/AjoutFormulaire.php
@@ -5,17 +5,25 @@ class AjoutFormulaire{
     public function generateAjoutForm(): void{?>
         <div class="ajout-recette">
             <form method="post" class="" id="ajout-recette-form"  enctype="multipart/form-data" action="<?php $GLOBALS['DOCUMENT_DIR'] ?>ajoutRecetteTraitement.php">
+<!--                Ajout des informations de la recette-->
                 <div class="TitleAjoutRecette">Ajouter une recette</div>
-                <input class = "ajout-input" type="text" id = "nom" name="nom" placeholder="Entrer le nom de la recette">
-                <label for="le_fichier" class="form-label">Nom de l'image avec l'extension :</label>
+                <input class = "ajout-input" type="text" id = "nom" name="nom" placeholder="Entrer le nom de la recette"> <!-- nom de la recette -->
+                <label for="le_fichier" class="form-label">Nom de l'image avec l'extension :</label> <!-- Image de la recette -->
                 <input type="file" class="file" id="photo_recette" name="photo_recette">
 
+<!--                liste des ingredients de la recette-->
                 <div id="ingredients">
                     <div class="titleIngredient">Ingrédients</div>
                     <div id="listeIngredient"></div>
                 </div>
+<!--                liste des tags de la recette-->
+                <div id="tags">
+                    <div class="titleTags">Tags</div>
+                    <div id="listeTags"></div>
+                </div>
             </form>
 
+<!--            Pour gerer l'ajout des ingredients-->
             <form  method="post" id = "ajout-ingredient-form" action=""  enctype="multipart/form-data">
 
                 <input class = "ajout-input" type="text" id = "nom-ingredient" name="nom-ingredient" placeholder="Entrer le nom de l'ingredient" value = "">
@@ -26,18 +34,30 @@ class AjoutFormulaire{
                     <input type="file" class="file" id="photo_ingredient" name="photo_ingredient">
                 </div>
                 <button type="button" id="ajouter-ingredient-button" class = "btn" >Ajouter un ingrédient</button>
-                <button type="button" id = "ajout-recette" class="btn" value="Ajouter la recette">Ajouter la recette</button>
-
             </form>
+
+<!--            Pour gerer l'ajout des tags-->
+            <form  method="post" id = "ajout-tag-form" action=""  enctype="multipart/form-data">
+                <input class = "ajout-input" type="text" id = "nom-tag" name="nom-tag" placeholder="Entrer un tag" value = "">
+                <button type="button" id="ajouter-tag-button" class = "btn" >Ajouter un tag</button>
+            </form>
+
+<!--            bouton pour l'ajout de la recette-->
+            <button type="button" id = "ajout-recette" class="btn" value="Ajouter la recette">Ajouter la recette</button>
+
             <script src = "<?= $GLOBALS['JS_DIR']?>admin.js"></script>
             <script>
-
-                let bt= document.getElementById("ajouter-ingredient-button");
-                bt.addEventListener("click", function(event){
+                // Script pour gerer l'ajout des tags et des recettes
+                let btIngredients= document.getElementById("ajouter-ingredient-button");
+                btIngredients.addEventListener("click", function(event){
                     // mets les codes php ici pour gerer l'ajout d'un ingredient
-                    <?php //echo 'alert("Ceci est une fenêtre pop-up !");'; ?>
+                    <?php echo 'alert("Ceci est une fenêtre ajout ingredients");'; ?>
+                })
 
-
+                let btTags= document.getElementById("ajouter-tag-button");
+                btTags.addEventListener("click", function(event){
+                    // mets les codes php ici pour gerer l'ajout des tags
+                    <?php echo 'alert("Ceci est une fenêtre Ajout tags !");'; ?>
                 })
             </script>
         </div>
