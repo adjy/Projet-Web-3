ae30f219fd4141af547d1c702b635862d3fc636a
RandRiaLau <ranlauren633@gmail.com>
2023-04-21 22:58:56 +0200

    affichage d une recette

---
+++ pages/afficheCategorie.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+require_once "../config.php";
+
+
+require $GLOBALS['PHP_DIR'] . "class/Autoloader.php";
+Autoloader::register();
+
+use recette\Template;
+use recette\AfficheRecette;
+use recette\Recette;
+
+
+$gdb = new Recette() ;
+$affiche = new AfficheRecette();
+$recettes = $gdb->getRecettes();
+$Listestag = $gdb->getListesTagRecettes();
+$tags = $gdb->getTagRecettes();
+
+ob_start();
+?>
+    <script src="../Javascript/main.js" ></script>
+    <link rel="stylesheet" href="../css/main.css">
+<?php
+
+if(isset($_POST['Id_Tag'])){
+    $affiche->ParCategorie($_POST['Id_Tag'],$Listestag,$tags,$recettes);
+}
+
+
+
+
+$content = ob_get_clean();
+Template::render($content, $title = "Ajout Recette");
