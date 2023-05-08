18aa1bb06d92d5038f49de3fbe30c1f5e5a65346
Sedar <desiradjysedar@gmail.com>
2023-05-08 19:00:07 +0200

    footer

--- pages/footer.php
+++ pages/footer.php
@@ -1,8 +1,25 @@
+<!--<footer id="footer">-->
+<!--       <div class="footCreateur">-->
+<!--                Ce site a été créé par Sedar, Lala et Astou-->
+<!--       </div>-->
+<!--       <div class="lien-source">-->
+<!--                Les ressources utilisés pour ce site proviennent <a href="https://www.buzzfeed.com/">buzzfeed </a>/ <a href="https://www.pinterest.com/">Pinterest</a>-->
+<!--       </div>-->
+<!--</footer>-->
+
+
 <footer id="footer">
-       <div class="footCreateur">
-                Ce site a été créé par Sedar, Lala et Astou
-       </div>
-       <div class="lien-source">
-                Les ressources utilisés pour ce site proviennent <a href="https://www.buzzfeed.com/">buzzfeed </a>/ <a href="https://www.pinterest.com/">Pinterest</a>
-       </div>
+
+    <img src="<?= $GLOBALS['IMG_DIR']?>src/tte.png" alt=""/>
+
+    <div class="footCreateur">
+        Pour tous renseignements, veuiller nous contacter à l'une des adresses suivantes;
+        <div class="mail"><a href="mailto:Astou.Gaye@etu.univ-littoral.fr" >  Astou.Gaye@etu.univ-littoral.fr</a></div>
+        <div class="mail"><a href="mailto:lala-britta-lau.randrianalisoa@etu.univ-littoral.fr">  lala-britta-lau.randrianalisoa@etu.univ-littoral.fr</a></div>
+        <div class="mail"><a href="mailto:Adjy-Sedar.Desir@etu.univ-littoral.fr" >  Adjy-Sedar.Desir@etu.univ-littoral.fr</a></div>
+    </div>
+
+    <div>
+        Les ressources utilisés pour ce site proviennent de <a href="https://www.buzzfeed.com/"> buzzfeed </a>/ <a href="https://www.pinterest.com/"> Pinterest</a>
+    </div>
 </footer>
