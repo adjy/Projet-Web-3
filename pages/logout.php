fa2c491fe2e5df3f0fd110d11fc6701b9986b680
RandRiaLau <ranlauren633@gmail.com>
2023-05-09 16:50:48 +0200

    cookie

--- pages/logout.php
+++ pages/logout.php
@@ -3,5 +3,7 @@ require __DIR__."/../config.php" ;
 
 session_start() ;
 session_destroy() ;
+$expire = time() - 3600 ;
+setcookie('username', '', $expire) ;
 header("Location: ".$GLOBALS['DOCUMENT_DIR']."index.php");
 exit() ;
