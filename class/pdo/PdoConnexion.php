3196c57c2789d1cdd1dc595826ef3bb02882917f
gaye018 <119873022+gaye018@users.noreply.github.com>
2023-05-12 06:55:33 +0200

    Update PdoConnexion.php

--- class/pdo/PdoConnexion.php
+++ class/pdo/PdoConnexion.php
@@ -17,16 +17,16 @@ class PdoConnexion
         $db_pwd = '' ;
         /*
                 machine fac :
-                $db_name = "lrandria" ;
+                $db_name = "agaye" ;
                 $db_host = '192.168.22.48' ;
                 $db_port = '3306' ;
-                $db_user =  'lrandria' ;
+                $db_user =  'agaye' ;
                 $db_pwd = '06032003' ;
 
-                $db_name = "lrandria" ;
+                $db_name = "agaye" ;
                 $db_host = '192.168.22.48' ;
                 $db_port = '3306' ;
-                $db_user =  'lrandria' ;
+                $db_user =  'agaye' ;
                 $db_pwd = '06032003' ;
         */
         $dsn = 'mysql:dbname=' . $db_name . ';host='. $db_host. ';port=' . $db_port;
